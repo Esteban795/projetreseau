d43a4b8f11b400c6c4606d2ffe6b48ddd26222ab
Esteban MANDJUKOWSKI <estebanmandjukowski@gmail.com>
2025-03-16 19:24:56 +0100

    feat: added TLS encryption

--- src/agent/agent.c
+++ src/agent/agent.c
@@ -1,5 +1,7 @@
 #include "../../include/agent/agent.h"
 
+#include <openssl/ssl.h>
+#include <openssl/err.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,8 +10,39 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-int main(int argc,const char* argv[]) {
 
+SSL_CTX* create_ssl_context() {
+    const SSL_METHOD* method;
+    SSL_CTX* ctx;
+
+    OpenSSL_add_ssl_algorithms();
+    method = SSLv23_server_method();
+    ctx = SSL_CTX_new(method);
+    if (!ctx) {
+        perror("Unable to create SSL context");
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    return ctx;
+}
+
+void configure_ssl_context(SSL_CTX* ctx) {
+    SSL_CTX_set_ecdh_auto(ctx, 1);
+
+    // Set the certificate and private key
+    if (SSL_CTX_use_certificate_file(ctx, "security/cert.crt", SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    if (SSL_CTX_use_PrivateKey_file(ctx, "security/key.key", SSL_FILETYPE_PEM) <= 0) {
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, const char* argv[]) {
     if (argc < 2) {
         printf("Usage: %s <port>\n", argv[0]);
         return 1;
@@ -39,7 +72,6 @@ int main(int argc,const char* argv[]) {
         return 1;
     }
 
-
     printf("[Agent] : Bound on port %hu\n", port);
 
     if (listen(server_fd, 10) < 0) {
@@ -49,6 +81,9 @@ int main(int argc,const char* argv[]) {
 
     printf("[Agent] : Listening on port %hu\n", port);
 
+    SSL_CTX* ctx = create_ssl_context();
+    configure_ssl_context(ctx);
+
     while (1) {
         struct sockaddr_in client_addr;
         socklen_t client_addr_len = sizeof(client_addr);
@@ -56,47 +91,66 @@ int main(int argc,const char* argv[]) {
         if (client_fd < 0) {
             printf("Error accepting connection\n");
             close(server_fd);
+            SSL_CTX_free(ctx);
             return 1;
         }
+
+        SSL* ssl = SSL_new(ctx);
+        SSL_set_fd(ssl, client_fd);
+
+        if (SSL_accept(ssl) <= 0) {
+            ERR_print_errors_fp(stderr);
+            SSL_free(ssl);
+            close(client_fd);
+            continue;
+        }
+
+        printf("[Agent] : SSL connection established\n");
 
         char command[MAX_BUFFER_SIZE];
         memset(command, 0, MAX_BUFFER_SIZE);
 
-        int n = read(client_fd, command, MAX_BUFFER_SIZE);
+        int n = SSL_read(ssl, command, MAX_BUFFER_SIZE);
         if (n <= 0) {
-            printf("Error reading from socket\n");
-            close(server_fd);
+            printf("Error reading from SSL connection\n");
+            ERR_print_errors_fp(stderr);
+            SSL_shutdown(ssl);
+            SSL_free(ssl);
             close(client_fd);
-            return 1;
+            continue;
         }
 
         printf("[Agent] : Received command '%s'\n", command);
 
-
-        // Exec command rcved by network, and grab output with popen
-
+        // Execute command received by network, and grab output with popen
         FILE* fp = popen(command, "r");
         if (fp == NULL) {
             printf("Error executing command\n");
+            SSL_shutdown(ssl);
+            SSL_free(ssl);
             close(client_fd);
-            return 1;
+            continue;
         }
 
         char cmd_output[MAX_BUFFER_SIZE];
         memset(cmd_output, 0, MAX_BUFFER_SIZE);
 
         while (fgets(cmd_output, MAX_BUFFER_SIZE, fp) != NULL) {
-            write(client_fd, cmd_output, strlen(cmd_output));
+            SSL_write(ssl, cmd_output, strlen(cmd_output));
             memset(cmd_output, 0, MAX_BUFFER_SIZE);
         }
 
         pclose(fp);
-        
+
         printf("[Agent] : Command output sent\n");
 
+        SSL_shutdown(ssl);
+        SSL_free(ssl);
         close(client_fd);
     }
 
     close(server_fd);
+    SSL_CTX_free(ctx);
+    EVP_cleanup();
     return 0;
 }
--- src/scheduler/scheduler.c
+++ src/scheduler/scheduler.c
@@ -2,7 +2,27 @@
 #include <arpa/inet.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
+SSL_CTX* create_ssl_context() {
+    const SSL_METHOD* method;
+    SSL_CTX* ctx;
+
+    OpenSSL_add_ssl_algorithms();
+    method = SSLv23_client_method();
+    ctx = SSL_CTX_new(method);
+    if (!ctx) {
+        perror("Unable to create SSL context");
+        ERR_print_errors_fp(stderr);
+        exit(EXIT_FAILURE);
+    }
+
+    return ctx;
+}
 
 Scheduler* scheduler_create(uint8_t num_agents) {
     Scheduler* scheduler = malloc(sizeof(Scheduler));
@@ -21,6 +41,7 @@ void scheduler_init(Scheduler* scheduler) {
     scheduler->agents = malloc(sizeof(Agent) * scheduler->num_agents);
     int i = 0;
     printf("[Scheduler] num_agents = %d\n", scheduler->num_agents);
+    // Scans each line formatted such as IP_ADDR PORT "command"
     while (fscanf(file, "%15s %d \"%255[^\"]\"\n",  scheduler->agents[i].ip, &scheduler->agents[i].port, scheduler->agents[i].commands) == 3) {
         printf("[Scheduler] : agent %s:%hu\n", scheduler->agents[i].ip, scheduler->agents[i].port);
         i++;
@@ -41,19 +62,22 @@ void scheduler_destroy(Scheduler* scheduler) {
     free(scheduler);
 }
 
-
 void* agent_run(void* agent_arg) {
-
     Agent* agent = (Agent*) agent_arg;
-    printf("[Agent] : %hhu.%hhu.%hhu.%hhu:%hu\n", agent->ip[0], agent->ip[1], agent->ip[2], agent->ip[3], agent->port);
+    printf("[Agent] : %s:%hu\n", agent->ip, agent->port);
 
     // Create TCP socket
-
-    int sockfd = socket(AF_INET,SOCK_STREAM,0);
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
         printf("[Agent] : Error creating socket\n");
         return NULL;
     }
+
+    // Init SSL context.
+    // Don't ask me how it works, I took it from openssl tutorial
+    SSL_CTX* ctx = create_ssl_context();
+    SSL* ssl = SSL_new(ctx);
+    SSL_set_fd(ssl, sockfd);
 
     struct sockaddr_in agent_addr;
     memset(&agent_addr, 0, sizeof(agent_addr));
@@ -63,38 +87,61 @@ void* agent_run(void* agent_arg) {
 
     if (connect(sockfd, (struct sockaddr*)&agent_addr, sizeof(agent_addr)) < 0) {
         printf("[Agent] : Error connecting to agent\n");
-        close(sockfd); // Necessary, because if it fails, the socket is not closed until a few minutes by the OS
+        close(sockfd);
+        SSL_free(ssl);
+        SSL_CTX_free(ctx);
+        return NULL;
+    }
+
+    if (SSL_connect(ssl) <= 0) {
+        ERR_print_errors_fp(stderr);
+        close(sockfd);
+        SSL_free(ssl);
+        SSL_CTX_free(ctx);
         return NULL;
     }
 
     printf("[Agent] : Connected to agent %s:%hu\n", agent->ip, agent->port);
 
-    ssize_t bytes_sent = write(sockfd,agent->commands,strlen(agent->commands));
-
-    if (bytes_sent < 0) {
+    int bytes_sent = SSL_write(ssl, agent->commands, strlen(agent->commands));
+    if (bytes_sent <= 0) {
         printf("[Agent] : Error sending commands\n");
+        ERR_print_errors_fp(stderr);
+        SSL_shutdown(ssl);
+        SSL_free(ssl);
         close(sockfd);
+        SSL_CTX_free(ctx);
         return NULL;
     }
 
-    printf("[Agent] : Sent %ld bytes\n", bytes_sent);
+    printf("[Agent] : Sent %d bytes\n", bytes_sent);
     printf("[Scheduler] Command '%s' sent to agent %s:%hu\n", agent->commands, agent->ip, agent->port);
-    
+
     char buffer[MAX_BUFFER_SIZE];
     memset(buffer, 0, MAX_BUFFER_SIZE);
 
     printf("[Scheduler] : Waiting for response from agent %s:%hu\n", agent->ip, agent->port);
 
     ssize_t bytes_rcved;
-    while ((bytes_rcved = read(sockfd, buffer, MAX_BUFFER_SIZE)) > 0) {
+    while ((bytes_rcved = SSL_read(ssl, buffer, MAX_BUFFER_SIZE)) > 0) {
         buffer[bytes_rcved] = '\0'; // Force to null-terminate the string
         printf("[Agent] : Received %ld bytes\n", bytes_rcved);
         printf("[Agent] : %s\n", buffer);
         memset(buffer, 0, sizeof(buffer));
     }
 
-    printf("[Scheduler] : Agent %hhu.%hhu.%hhu.%hhu:%hu finished\n", agent->ip[0], agent->ip[1], agent->ip[2], agent->ip[3], agent->port);
+    if (bytes_rcved < 0) {
+        printf("[Agent] : Error receiving response\n");
+        ERR_print_errors_fp(stderr);
+    }
+
+    printf("[Scheduler] : Agent %s:%hu finished\n", agent->ip, agent->port);
+
+    SSL_shutdown(ssl);
+    SSL_free(ssl);
     close(sockfd);
+    SSL_CTX_free(ctx);
+
     return NULL;
 }
 
